8814df76d5cf1072448ba4a913dba4edf55eb3d3
caleb <caleb@prowebmarketing.com>
2016-06-21 00:40:46 -0400

    Chassis working?

--- lib/myGpio.py
+++ lib/myGpio.py
@@ -56,6 +56,7 @@ def pwmPinsUpdate(pin, freq, dutyCycle):
     @param float dutyCycle
         (0.0 <= dc <= 100.0)
     """
+    print("pwmPinsUpdate", pin, freq, dutyCycle, pwmPins.get(pin))
     if( pwmPins.get(pin) ):
         pwmPins.get(pin).ChangeDutyCycle(dutyCycle)
         pwmPins.get(pin).ChangeFrequency(freq)
