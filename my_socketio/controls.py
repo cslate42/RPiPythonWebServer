80bb5d9bd989471e72eba91716e03e864522f194
caleb <caleb@prowebmarketing.com>
2016-06-21 00:21:12 -0400

    Chassis working?

--- my_socketio/controls.py
+++ my_socketio/controls.py
@@ -10,6 +10,7 @@ def updateControls(sid, message):
     myGlobals.sio.emit('update-controls-results', {'message': message, 'sid': sid}, namespace='/socketio')
     keysPressed = message['keysPressed'] if message['keysPressed'] else {}
 
+    print(keysPressed)
     # ---------------------------------------CHASSIS CONTROLS--------------------------------------------------------
     if( keysPressed.get('ArrowUp') and keysPressed.get('ArrowLeft') ):
         chassisForwardLeft()
