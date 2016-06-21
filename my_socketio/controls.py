7f80aff7a586675999ee7e04a5daa800fc586999
caleb <caleb@prowebmarketing.com>
2016-06-20 22:33:01 -0400

    Chassis working?

--- my_socketio/controls.py
+++ my_socketio/controls.py
@@ -13,94 +13,93 @@ def updateControls(sid, message):
     keysPressed = message['keysPressed'] if message['keysPressed'] else []
 
     # ------------------CHASSIS CONTROLS---------------------------------
-    if( keysPressed['ArrowUp'] and keysPressed['ArrowLeft'] ) {
-        chassisForwardLeft();
-    } elif( keysPressed['ArrowUp'] and keysPressed['ArrowRight'] ) {
-        chassisForwardRight();
-    } elif( keysPressed['ArrowDown'] and keysPressed['ArrowLeft'] ) {
-        chassisBackwardLeft();
-    } elif( keysPressed['ArrowDown'] and keysPressed['ArrowRight'] ) {
-        chassisBackwardRight();
-    } elif ( (keysPressed['ArrowUp'] and keysPressed['ArrowDown']) || (keysPressed['ArrowLeft'] and keysPressed['ArrowRight']) ) {
-        chassisStop();
-    } elif ( keysPressed['ArrowUp'] ) {
-        chassisForward();
-    } elif ( keysPressed['ArrowRight'] ) {
-        chassisRight();
-    } elif ( keysPressed['ArrowDown'] ) {
-        chassisBackward();
-    } elif ( keysPressed['ArrowLeft'] ) {
-        chassisLeft();
-    } else {
-        chassisStop();
-    }
+    if( keysPressed['ArrowUp'] and keysPressed['ArrowLeft'] ):
+        chassisForwardLeft()
+    elif( keysPressed['ArrowUp'] and keysPressed['ArrowRight'] ):
+        chassisForwardRight()
+    elif( keysPressed['ArrowDown'] and keysPressed['ArrowLeft'] ):
+        chassisBackwardLeft()
+    elif( keysPressed['ArrowDown'] and keysPressed['ArrowRight'] ):
+        chassisBackwardRight()
+    elif ( (keysPressed['ArrowUp'] and keysPressed['ArrowDown']) || (keysPressed['ArrowLeft'] and keysPressed['ArrowRight']) ):
+        chassisStop()
+    elif ( keysPressed['ArrowUp'] ):
+        chassisForward()
+    elif ( keysPressed['ArrowRight'] ):
+        chassisRight()
+    elif ( keysPressed['ArrowDown'] ):
+        chassisBackward()
+    elif ( keysPressed['ArrowLeft'] ):
+        chassisLeft()
+    else:
+        chassisStop()
 
     //-----------------------------LED TEST----------------------------
-    if( keysPressed['a'] ) {
-        myGPIO.write(3, 1);
-    } else {
-        myGPIO.write(3, 0);
+    if( keysPressed['a'] ):
+        myGPIO.write(3, 1)
+    else {
+        myGPIO.write(3, 0)
     }
-};
+}
 
 
 def chassisForwardLeft():
     # TODO pwm
-    myGPIO.write(myGPIO.MOTOR_L_B, 0);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 0);
+    myGPIO.write(myGPIO.MOTOR_L_B, 0)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 0)
 
 def chassisForwardRight():
     # TODO pwm
-    myGPIO.write(myGPIO.MOTOR_L_B, 1);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 1);
+    myGPIO.write(myGPIO.MOTOR_L_B, 1)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 1)
 
 def chassisBackwardLeft():
     #TODO pwm
-    myGPIO.write(myGPIO.MOTOR_L_B, 1);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 1);
+    myGPIO.write(myGPIO.MOTOR_L_B, 1)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 1)
 
 def chassisBackwardRight():
     #TODO pwm
-    myGPIO.write(myGPIO.MOTOR_L_B, 1);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 1);
+    myGPIO.write(myGPIO.MOTOR_L_B, 1)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 1)
 
 def chassisForward():
-    myGPIO.write(myGPIO.MOTOR_L_B, 0);
-    myGPIO.write(myGPIO.MOTOR_L_F, 1);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 1);
+    myGPIO.write(myGPIO.MOTOR_L_B, 0)
+    myGPIO.write(myGPIO.MOTOR_L_F, 1)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 1)
 
 def chassisRight():
-    myGPIO.write(myGPIO.MOTOR_L_B, 0);
-    myGPIO.write(myGPIO.MOTOR_L_F, 1);
-    myGPIO.write(myGPIO.MOTOR_R_B, 1);
-    myGPIO.write(myGPIO.MOTOR_R_F, 0);
+    myGPIO.write(myGPIO.MOTOR_L_B, 0)
+    myGPIO.write(myGPIO.MOTOR_L_F, 1)
+    myGPIO.write(myGPIO.MOTOR_R_B, 1)
+    myGPIO.write(myGPIO.MOTOR_R_F, 0)
 
 def chassisBackward():
-    myGPIO.write(myGPIO.MOTOR_L_B, 1);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 1);
-    myGPIO.write(myGPIO.MOTOR_R_F, 0);
+    myGPIO.write(myGPIO.MOTOR_L_B, 1)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 1)
+    myGPIO.write(myGPIO.MOTOR_R_F, 0)
 
 def chassisLeft():
-    myGPIO.write(myGPIO.MOTOR_L_B, 1);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 1);
+    myGPIO.write(myGPIO.MOTOR_L_B, 1)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 1)
 
 def chassisStop():
-    myGPIO.write(myGPIO.MOTOR_L_B, 0);
-    myGPIO.write(myGPIO.MOTOR_L_F, 0);
-    myGPIO.write(myGPIO.MOTOR_R_B, 0);
-    myGPIO.write(myGPIO.MOTOR_R_F, 0);
+    myGPIO.write(myGPIO.MOTOR_L_B, 0)
+    myGPIO.write(myGPIO.MOTOR_L_F, 0)
+    myGPIO.write(myGPIO.MOTOR_R_B, 0)
+    myGPIO.write(myGPIO.MOTOR_R_F, 0)
 
 # @myGlobals.app.route('/controls')
 # def control():
