b45cdebab6d46798ee0a214b2283ec1f0a0272bd
caleb <caleb@prowebmarketing.com>
2016-06-20 21:57:10 -0400

    update

--- lib/myGpio.py
+++ lib/myGpio.py
@@ -1,23 +1,23 @@
 import RPi.GPIO as GPIO
 
+#===============================+START SETUP+=============================
 MOTOR_L_F = 35
 MOTOR_L_B = 36
 MOTOR_R_F = 37
 MOTOR_R_B = 38
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
-    GPIO.setup(MOTOR_L_F, GPIO.OUT)
-    GPIO.setup(MOTOR_L_B, GPIO.OUT)
-    GPIO.setup(MOTOR_R_F, GPIO.OUT)
-    GPIO.setup(MOTOR_R_B, GPIO.OUT)
+GPIO.setup(MOTOR_L_F, GPIO.OUT)
+GPIO.setup(MOTOR_L_B, GPIO.OUT)
+GPIO.setup(MOTOR_R_F, GPIO.OUT)
+GPIO.setup(MOTOR_R_B, GPIO.OUT)
 
-    GPIO.output(MOTOR_L_F, False)
-    GPIO.output(MOTOR_L_B, False)
-    GPIO.output(MOTOR_R_F, False)
-    GPIO.output(MOTOR_R_B, False)
-
+GPIO.output(MOTOR_L_F, False)
+GPIO.output(MOTOR_L_B, False)
+GPIO.output(MOTOR_R_F, False)
+GPIO.output(MOTOR_R_B, False)
+#===============================+END SETUP+=============================
 def write(pin, state):
     """
     Write output GPIO pin to state MAKE SURE pin is setup before use
