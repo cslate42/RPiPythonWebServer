190fa3a05c18c246ce4c70e847d00e546561d8e3
caleb <caleb@prowebmarketing.com>
2016-06-20 22:01:54 -0400

    update

--- lib/myGpio.py
+++ lib/myGpio.py
@@ -24,7 +24,7 @@ def write(pin, state):
     @param int pin
     @param bool state
     """
-    gpioState = GPIO.HIGH if state === True else GPIO.LOW
+    gpioState = GPIO.HIGH if state == True else GPIO.LOW
     print("GPIO WRITE", pin, state, gpioState)
     GPIO.output(pin, gpioState)
 
